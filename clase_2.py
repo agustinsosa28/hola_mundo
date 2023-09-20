47f6c1d2f6e2c4b19a2c348f3d3ff427789e2199
agustingsosa <agsosa44@gmail.com>
2023-09-20 19:57:47 -0300

    123

---
+++ clase_2.py
@@ -0,0 +1,11 @@
+print("hola")
+mi_nombre = "caro"
+print(mi_nombre)
+apellido = "sosa"
+print(len(apellido))
+numeros = "123456"
+print(numeros[0])
+notas_del_primer_parcial = input("ingrese nota del primer parcial")
+notas_del_segundo_parcial = input("ingrese nota del segundo parcial")
+print("nota del primer parcial:" + notas_del_primer_parcial)
+print("nota del segundo parcial:" + notas_del_segundo_parcial)
